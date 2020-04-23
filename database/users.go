07f05c8782690122e55780e89c900ef8ba0d17d7
Jae Bradley <jae.b.bradley@gmail.com>
2020-04-22 23:17:53 -0400

    Add user resource creation

--- database/users.go
+++ database/users.go
@@ -3,7 +3,9 @@ package database
 import (
 	"fmt"
 	"net/mail"
+	"net/url"
 	"os"
+	"time"
 )
 
 const (
@@ -14,6 +16,14 @@ const (
 type User struct {
 	ID           uint64
 	EmailAddress string
+}
+
+// UserResource represents a resource associated with the specified User
+type UserResource struct {
+	ID        uint64
+	UserID    uint64
+	Location  string
+	CreatedAt uint64
 }
 
 // CreateUser adds an application user to the database
@@ -110,4 +120,85 @@ func GetUserByID(id uint64) User {
 	}
 
 	return user
+}
+
+// CreateUserResource creates a URL associated with a user
+func CreateUserResource(user *User, url *url.URL) (*UserResource, error) {
+	conn, err := ConnectionPool.Acquire()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to connect to database : %v\n", err)
+	}
+
+	defer conn.Close()
+
+	var userResource UserResource
+
+	err = conn.QueryRow(`
+		INSERT INTO user_resources (user_id, location, created_at)
+		VALUES ($1, $2, $3)
+		RETURNING
+			"id",
+			"user_id",
+			"location",
+			"created_at"`,
+		user.ID,
+		url.String(),
+		time.Now().UTC(),
+	).Scan(
+		&userResource.ID,
+		&userResource.UserID,
+		&userResource.Location,
+		&userResource.CreatedAt,
+	)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed : %v\n", err)
+	}
+
+	return &userResource, nil
+}
+
+// GetUserResources returns a list of resources for the specified user
+func GetUserResources(user *User) (*[]UserResource, error) {
+	conn, err := ConnectionPool.Acquire()
+
+	defer conn.Close()
+
+	userResources := []UserResource{}
+
+	rows, err := conn.Query(`
+		SELECT
+			id,
+			user_id,
+			location,
+			created_at
+		FROM user_resources
+		WHERE user_id = $1`,
+		user.ID,
+	)
+
+	defer rows.Close()
+
+	// Iterate through the result set
+	for rows.Next() {
+		var userResource UserResource
+		err = rows.Scan(
+			&userResource.ID,
+			&userResource.UserID,
+			&userResource.Location,
+			&userResource.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		userResources = append(userResources, userResource)
+	}
+
+	if rows.Err() != nil {
+		return nil, err
+	}
+
+	return &userResources, nil
 }
